44bbfc46f4d50f92d1fc1431e4a5ba639cd1b519
shark2302 <58619590+shark2302@users.noreply.github.com>
2020-05-04 23:05:32 +0300

    Initial commit

---
+++ Assets/LiftController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftController : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("open");
+            _animator.SetBool("open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Close");
+            _animator.SetBool("open", false);
+        }
+    }
+}
